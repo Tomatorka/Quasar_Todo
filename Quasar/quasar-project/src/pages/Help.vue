81feb3c01c6833bccd7e7702fee9b7c641834a01
Letuchaya_koshka <164854934+Tomatorka@users.noreply.github.com>
2024-03-30 16:10:51 +0300

    Quasar (Todo)

---
+++ Quasar/quasar-project/src/pages/Help.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, est. Nesciunt magnam sequi maxime! Ratione quo dolorem neque qui ad tempora eum suscipit numquam, expedita natus quidem obcaecati ut alias!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat maxime exercitationem sed accusamus soluta? Similique harum veritatis rerum rem officia sapiente repudiandae esse iste! Beatae reprehenderit dolorem optio dolor a?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, exercitationem nihil adipisci possimus temporibus cumque repellendus quidem mollitia reiciendis at odit recusandae accusantium alias quasi debitis modi laboriosam eos voluptas.</p>
+  </q-page>
+</template>
+
+<script setup>
+defineOptions({
+  name: 'IndexPage'
+});
+</script>
